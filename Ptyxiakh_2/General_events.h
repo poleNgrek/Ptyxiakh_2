9ec4d34edecfba225f3f48bdd318f33c719b3bca
Karolos Triantafyllou <charlie.roseman@yahoo.com>
2015-04-11 20:51:31 +0300

    Events and States changes
    Added C-style strings that hold the names of the aforementioned and a
    function to return the name in text format.

--- Ptyxiakh_2/General_events.h
+++ Ptyxiakh_2/General_events.h
@@ -2,9 +2,10 @@
   * General_events.h
   *
   * Here are stored the enumerated class events for the simulation, the cores
-  * and the dispatchers.
+  * and the dispatchers. The header file also provides a C-style array to hold
+  * the names of the enums and a function to return that name.
   *
-  * Version 0.5
+  * Version 0.9
   * Last edit: 11/09/2015
   *
   * K. Triantafyllou
@@ -25,5 +26,20 @@ enum class Events
     CORE_TOPOLOGY,
     CORE_JOB
 };
+
+// Simple C-style array with C-strings as names for the enums.
+static const char* event_string[] =
+{
+    "SIM_START", "SIM_STOP", "SIM_END", "DISP_JOB", "DISP_IDLE",
+    "CORE_TOPOLOGY", "CORE_JOB"
+};
+
+// Simple function that takes a Events enum class and returns the C-style array.
+// Note: enum class cannot be implicitly converted to int, thus a static_cast
+// is needed.
+const char* event_to_text(Events events)
+{
+    return event_string[static_cast<int>(events)];
+}
 
 #endif
