3267b1cca48221a0d596e0a01a3ba33caa77565d
Karolos Triantafyllou <charlie.roseman@yahoo.com>
2015-04-15 20:44:15 +0300

    Redo previous commit

---
+++ Ptyxiakh_2/Functions.cpp
@@ -0,0 +1,16 @@
+#include "Functions.h"
+
+#include "Simulation.h"
+#include "General_events.h"
+
+void simulation_event_generator(Simulation& simulation)
+{
+    bool flag = false;
+    
+    while (!flag)
+    {
+        // TODO
+        // Add lambda expression
+        // Run in own thread
+    }
+}
---
+++ Ptyxiakh_2/Functions.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+// Forward declaration
+class Simulation;
+
+void simulation_event_generator(Simulation& simulation);
+
+#endif
